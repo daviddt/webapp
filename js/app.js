32d57e9b02569d5746a5f33558f1cad39fc01efe
daviddt <daviddentoom@gmail.com>
2013-10-28 10:51:36 +0100

    first upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var FrisbeeApp = angular.module('frisbee', ['angular-gestures']);
+
+(function(){
+
+	FrisbeeApp.config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
+	  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+	  $routeProvider.
+	      when('/home', {templateUrl: 'partials/home.html',   controller: 'HomeCtrl'}).
+	      when('/pools', {templateUrl: 'partials/pools.html',   controller: 'PoolCtrl'}).
+	      when('/team/:teamID', {templateUrl: 'partials/team.html',   controller: 'TeamCtrl'}).
+	      when('/match/:matchID', {templateUrl: 'partials/match.html',   controller: 'MatchCtrl'}).
+	      when('/addscore/:matchID', {templateUrl: 'partials/addscore.html',   controller: 'ScoreCtrl'}).
+	      otherwise({redirectTo: '/home'});
+	}]);
+
+})();
